ca27004d730d0bbb60f63737700f14ee942c6fe4
Dom Goodwin <dom.goodwin@capgemini.com>
2018-08-07 11:28:39 +0100

    rebuilding site Tue  7 Aug 2018 11:28:39 BST

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,14 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
+msg="rebuilding site `date`"
+git add .
+git commit -m "$msg"
 cd public
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
